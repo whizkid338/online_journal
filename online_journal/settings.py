6f400b0ae4613599bb2babdfa4faf833b3879f50
Darian Ramage <darianramage@yahoo.com>
2013-12-06 00:43:31 +0000

    Moved style.css to static folder, finished config for static files

--- online_journal/settings.py
+++ online_journal/settings.py
@@ -85,10 +85,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_URL = '/online_journal/static/'
+STATICFILES_DIRS = (
+	os.path.join(BASE_DIR, 'static'),
+)
 
-STATIC_URL = '/static/'
-
-MEDIA_ROOT = '/home/ubuntu/online_journal/static/'
+# MEDIA_ROOT = '/home/ubuntu/online_journal/static/'
 
 # Template Location
 TEMPLATE_DIRS = (
