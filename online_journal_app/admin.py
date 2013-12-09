3342bef55438b96d03970fb9fa79d979c8008531
Harodan <harodan@harodan-Inspiron-1545.(none)>
2013-12-08 22:31:41 -0700

    Added online_journal_app models and settings

--- online_journal_app/admin.py
+++ online_journal_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from oj.models import *
 
 # Register your models here.
+admin.site.register(Entry)
+admin.site.register(Tag)
+admin.site.register(Author)
--- online_journal_app/models.py
+++ online_journal_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+	def __unicode__(self):
+		return self.name
+	
+	auth_identifier = models.CharField(max_length=255)
+	name = models.CharField(max_length=255)
+
+class Entry(models.Model):
+	def __unicode__(self):
+		return self.title
+
+	author = models.ForeignKey(Author)
+	title = models.CharField(max_length=255)
+	content = models.TextField()
+	pub_date = models.DateTimeField('Date of Entry')
+
+class Tag(models.Model):
+	def __unicode__(self):
+		return self.tag_name
+
+	entry = models.ForeignKey(Entry)
+	tag_name = models.CharField(max_length=50)
